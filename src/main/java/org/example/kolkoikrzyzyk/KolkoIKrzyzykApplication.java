292b89503cf70c3e1c6a4027fbe7b243a820ccc1
Aleksandra Kot <aleksandrak.kot@gmail.com>
2024-05-27 20:43:37 +0200

    First Commit

---
+++ src/main/java/org/example/kolkoikrzyzyk/KolkoIKrzyzykApplication.java
@@ -0,0 +1,13 @@
+package org.example.kolkoikrzyzyk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KolkoIKrzyzykApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KolkoIKrzyzykApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/kolkoikrzyzyk/KolkoIKrzyzykApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kolkoikrzyzyk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KolkoIKrzyzykApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
